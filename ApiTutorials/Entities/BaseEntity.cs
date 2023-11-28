4bd5118dd4ea37b4fd008d1d30a93727a58ffcfc
berdikulov-571 <mygithub571@gmail.com>
2023-11-28 14:04:43 +0500

    BaseEntity

---
+++ ApiTutorials/Entities/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiTutorials.Entities
+{
+    public class BaseEntity : Auditable
+    {
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
