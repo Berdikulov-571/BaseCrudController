68b922abecd68cafb5648d93f27b9f31461205e6
berdikulov-571 <mygithub571@gmail.com>
2023-11-28 14:03:31 +0500

    AutoMapper

---
+++ ApiTutorials/AutoMapper/MapProfile.cs
@@ -0,0 +1,16 @@
+﻿using ApiTutorials.DTOs;
+using ApiTutorials.Entities.Users;
+using AutoMapper;
+
+namespace ApiTutorials.AutoMapper
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<User, UserCreateDto>().ReverseMap();
+            CreateMap<User, UserUpdateDto>().ReverseMap();
+            CreateMap<User, UserResultDto>().ReverseMap();
+        }
+    }
+}
