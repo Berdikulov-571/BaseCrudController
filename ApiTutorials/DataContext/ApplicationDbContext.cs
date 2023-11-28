090f2057774d2e019f19dcea55c4a6b038190d0a
berdikulov-571 <mygithub571@gmail.com>
2023-11-28 14:03:56 +0500

    ApplicationDbContext

---
+++ ApiTutorials/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiTutorials.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiTutorials.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
