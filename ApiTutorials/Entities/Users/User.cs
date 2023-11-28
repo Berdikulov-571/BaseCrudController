0ade48f9f9f63e7204c5ed733e679a6cdca29f4f
berdikulov-571 <mygithub571@gmail.com>
2023-11-28 14:04:30 +0500

    UserModel

---
+++ ApiTutorials/Entities/Users/User.cs
@@ -0,0 +1,16 @@
+﻿using ApiTutorials.Enums;
+using System.Text.Json.Serialization;
+
+namespace ApiTutorials.Entities.Users
+{
+    public class User : BaseEntity
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string UserName { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public int Age { get; set; } = default!;
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Role Role { get; set; } = default!;
+    }
+}
