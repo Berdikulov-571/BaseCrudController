f36a6a083f2e9afe04734408ac673481ca170f32
berdikulov-571 <mygithub571@gmail.com>
2023-11-28 14:04:15 +0500

    UserUpdateDto

---
+++ ApiTutorials/DTOs/UserUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using ApiTutorials.Entities;
+using ApiTutorials.Enums;
+
+namespace ApiTutorials.DTOs
+{
+    public class UserUpdateDto : Auditable
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string UserName { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public int Age { get; set; } = default!;
+        public Role Role { get; set; } = default!;
+    }
+}
