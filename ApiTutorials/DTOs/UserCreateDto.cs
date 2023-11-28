e4b651cb5ed034f7a29243b3dd7b878990557291
berdikulov-571 <mygithub571@gmail.com>
2023-11-28 14:04:06 +0500

    UserCreateionDto

---
+++ ApiTutorials/DTOs/UserCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using ApiTutorials.Enums;
+
+namespace ApiTutorials.DTOs
+{
+    public class UserCreateDto
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string UserName { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public int Age { get; set; } = default!;
+        public Role Role { get; set; } = default!;
+    }
+}
