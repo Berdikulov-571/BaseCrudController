6a2a6d8c30bed2c2501c50c6616eb90e0fd7dbfe
berdikulov-571 <mygithub571@gmail.com>
2023-11-28 14:04:48 +0500

    Auditable

---
+++ ApiTutorials/Entities/Auditable.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiTutorials.Entities
+{
+    public class Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
