712608a005bfb01686e033244488862c7498354d
berdikulov-571 <mygithub571@gmail.com>
2023-11-28 14:04:36 +0500

    Migrations

---
+++ ApiTutorials/Migrations/20231128083021_Modified.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTutorials.Migrations
+{
+    /// <inheritdoc />
+    public partial class Modified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
