a30becd1a2ab3aaab51b6e291f6c00e21361c6db
berdikulov-571 <mygithub571@gmail.com>
2023-11-28 14:03:46 +0500

    UserController

---
+++ ApiTutorials/Controllers/UsesrControllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using ApiTutorials.DataContext;
+using ApiTutorials.DTOs;
+using ApiTutorials.Entities.Users;
+using AutoMapper;
+
+namespace ApiTutorials.Controllers.UsesrControllers
+{
+    public class UserController : BaseCRUDController<UserCreateDto, UserUpdateDto, UserResultDto, User>
+    {
+        public UserController(ApplicationDbContext context, IMapper mapper) : base(context, mapper)
+        {
+
+        }
+    }
+}
